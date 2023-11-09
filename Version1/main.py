9006cb6085554ff04631ae023c784929dc5ba646
Hossein <hossein.ghermezcheshme@gmail.com>
2023-11-09 09:33:21 +0330

    complete game

--- Version1/main.py
+++ Version1/main.py
@@ -1,11 +1,9 @@
-from typing import List
-
 print("Hello everyone")
 print("Welcome to Tic-tac-toe game:)\n")
 
-board: list[list[str]] = [['1', '2', '3'],
-                          ['4', '5', '6'],
-                          ['7', '8', '9']]
+board: list[list[str]] = [[" ", " ", " "],
+                          [" ", " ", " "],
+                          [" ", " ", " "]]
 
 scores = {1: "X", -1: "O", 0: "tie"}
 
@@ -27,13 +25,39 @@ class Game:
             print("")
 
 
+def bestMove():
+    move = (0, 0)
+    bestScore = float('-inf')
+    for i in range(3):
+        for j in range(3):
+            # if board[i][j] != "O" and board[i][j] != "X":
+            if board[i][j] == " ":
+                board[i][j] = "X"
+                score = maxmin(board, 0, False)
+                # board[i][j] = str((i * 3) + j + 1)
+                board[i][j] = " "
+                if score > bestScore:
+                    bestScore = score
+                    move = (i, j)
+    board[move[0]][move[1]] = "X"
+
+
+def isFull(board):
+    for i in range(3):
+        for j in range(3):
+            # if board[i][j] != "X" and board[i][j] != "O":
+            if board[i][j] == " ":
+                return False
+    return True
+
+
 def checkWinner(board):
     tempString3 = board[0][0] + board[1][1] + board[2][2]
-    tempString4 = board[0][2] + board[1][1] + board[2][0]
+    tempString4 = board[2][0] + board[1][1] + board[0][2]
 
     if tempString3 == "XXX" or tempString4 == "XXX":
         return 1
-    elif tempString3 == "XXX" or tempString4 == "XXX":
+    elif tempString3 == "OOO" or tempString4 == "OOO":
         return -1
 
     for i in range(3):
@@ -46,7 +70,7 @@ def checkWinner(board):
             return 1
         elif tempString1 == "OOO" or tempString2 == "OOO":
             return -1
-        elif i == 2 and j == 2:
+        elif isFull(board):
             return 0
     return 2
 
@@ -57,47 +81,51 @@ def maxmin(board1, depth, isMaximizing):
         return result
 
     if isMaximizing:
-        bestScore = -2
+        bestScore = float('-inf')
         for i in range(3):
             for j in range(3):
-                if board1[i][j] != "O" or board1[i][j] != "X":
+                # if board1[i][j] != "O" and board1[i][j] != "X":
+                if board1[i][j] == " ":
                     board1[i][j] = "X"
                     score = maxmin(board1, depth + 1, False)
-                    board1[i][j] = (i * 3) + j + 1
+                    # board1[i][j] = str((i * 3) + j + 1)
+                    board1[i][j] = " "
                     bestScore = max(score, bestScore)
         return bestScore
     else:
-        bestScore = 2
+        bestScore = float('inf')
         for i in range(3):
             for j in range(3):
-                if board1[i][j] != "O" or board1[i][j] != "X":
+                # if board1[i][j] != "O" and board1[i][j] != "X":
+                if board1[i][j] == " ":
                     board1[i][j] = "O"
                     score = maxmin(board1, depth + 1, True)
-                    board1[i][j] = (i * 3) + j + 1
+                    # board1[i][j] = str((i * 3) + j + 1)
+                    board1[i][j] = " "
                     bestScore = min(score, bestScore)
         return bestScore
 
 
 game = Game(board)
+
 turn = 1
 while True:
+
     game.printBoard()
-
-    if turn == 1:
-        maxmin(board, 1, True)
-    else:
-        print("Enter Your choice ", ":", end="")
-        x = int(input())
-        y = int(input())
-
-        board[x][y] = "O"
-
-        if checkWinner(board) != 2:
-            break
-
     print("\n")
 
-# print(maxmin(board, 1, True))
+    if checkWinner(board) != 2:
+        print(checkWinner(board))
+        break
 
+    if turn == 1:
+        bestMove()
+        turn = 0
+        continue
+    else:
+        print("Enter Your choice ", ":", end="")
+        number = int(input())
 
-
+        board[(number - 1) // 3][(number - 1) % 3] = "O"
+        turn = 1
+        continue
